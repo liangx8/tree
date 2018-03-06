24ea1b077aedfa03785f2595db578a5de153dcfe
wang gang <wang.home.gang@linuxbox.com>
2018-03-06 22:18:56 +0800

    random data in NewSimpleModel

--- treeview/model.go
+++ treeview/model.go
@@ -26,15 +26,19 @@ type sampleModel struct {
 }
 
 func (m *sampleModel) ObjectWidth() int {
-	return 4
+	return 2
 }
 func (m *sampleModel) ObjectStr(o interface{}) string {
+	if o == (*sampleModel)(nil) {
+		return "**"
+	}
 	return o.(*sampleModel).name
 }
 func (m *sampleModel) Root() interface{} {
 	return m
 }
 func (m *sampleModel) ChildCount(parent interface{}) int {
+	if parent==(*sampleModel)(nil) {return 0}
 	return len(parent.(*sampleModel).children)
 }
 func (m *sampleModel) Child(parent interface{}, idx int) interface{} {
@@ -42,49 +46,28 @@ func (m *sampleModel) Child(parent interface{}, idx int) interface{} {
 }
 
 // Create a Sample model
-func NewSampleModel() Model {
-	sm := &sampleModel{name: "root", children: nil}
-	sm.children = make([]*sampleModel, 4)
-	sm.children[0] = &sampleModel{name: "[c1]", children: make([]*sampleModel, 3)}
-	sm.children[1] = &sampleModel{name: "[c2]", children: nil}
-	sm.children[2] = &sampleModel{name: "[c3]", children: make([]*sampleModel, 2)}
-	sm.children[3] = &sampleModel{name: "[c4]", children: make([]*sampleModel, 2)}
 
-	sm.children[0].children[0] = &sampleModel{name: "[k1]", children: nil}
-	sm.children[0].children[1] = &sampleModel{name: "[k2]", children: nil}
-	sm.children[0].children[2] = &sampleModel{name: "[k3]", children: nil}
-
-	sm.children[0].children[0].children = make([]*sampleModel, 2)
-	sm.children[0].children[0].children[0] = &sampleModel{name: "[x1]", children: nil}
-	sm.children[0].children[0].children[1] = &sampleModel{name: "[x2]", children: nil}
-
-	sm.children[0].children[2].children = make([]*sampleModel, 4)
-	sm.children[0].children[2].children[0] = &sampleModel{name: "[x3]", children: nil}
-	sm.children[0].children[2].children[1] = &sampleModel{name: "[x4]", children: nil}
-	sm.children[0].children[2].children[2] = &sampleModel{name: "[x5]", children: nil}
-	sm.children[0].children[2].children[3] = &sampleModel{name: "[x6]", children: nil}
-
-	sm.children[2].children[0] = &sampleModel{name: "[k4]", children: nil}
-	sm.children[2].children[1] = &sampleModel{name: "[k5]", children: nil}
-	sm.children[3].children[0] = &sampleModel{name: "[k6]", children: nil}
-	sm.children[3].children[1] = &sampleModel{name: "[k7]", children: nil}
-
-	return sm
-}
-func RandSampleModel() Model {
-	sm := &sampleModel{name: "root", children: nil}
-	var build func(*sampleModel,rune,int,int)
-	build=func(parent *sampleModel,prefix rune,cnt,level int){
+func NewSampleModel(lvl int) Model {
+	if lvl > 9 {lvl = 9}
+	sm := &sampleModel{name: "RT", children: nil}
+	var build func(*sampleModel,rune,int)
+	build=func(parent *sampleModel,prefix rune,level int){
 		if level == 0 { return }
+		cnt:=rand.Int() % 5
+		if cnt < 0 { cnt = -cnt}
+		cnt ++
 		parent.children =make([]*sampleModel,cnt)
-		fmt.Printf("%d,",len(parent.children))
-
-		for i,_ := range sm.children {
-			sm.children[i]=&sampleModel{name:fmt.Sprintf("[%c%d]",prefix,i),children:nil}
-			build(sm.children[i],prefix + 1,rand.Int() %6,level -1)
+		fmt.Printf("count:%d\n",cnt)
+		for i,_ := range parent.children {
+			r := rand.Int() % 20
+			fmt.Println(r)
+			if r < 19 {
+				parent.children[i]=&sampleModel{name:fmt.Sprintf("%c%d",prefix,i),children:nil}
+				build(parent.children[i],prefix + 1,level -1)
+			}
 		}
 	}
-	build(sm,'a',rand.Int() % 6+4,4)
-	
+	build(sm,'a',lvl)
+
 	return sm
 }
